8aecbfd4d6d35801cb2aa5679632ae37d58cf273
aleksashenka <aleksashenka@gmail.com>
2015-04-14 22:48:13 +0300

    Initial commit

---
+++ app/src/main/java/ee/aleksashenka/fourscreens/MainScreenActivity.java
@@ -0,0 +1,54 @@
+package ee.aleksashenka.fourscreens;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainScreenActivity extends ActionBarActivity  implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+        View secondScreen = this.findViewById(R.id.FirstScreenButton);
+        secondScreen.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+        }
+
+    @Override
+    public void onClick(View arg0) {
+        if(arg0.getId() == R.id.FirstScreenButton){
+            //define a new Intent for the second Activity
+            Intent intent = new Intent(this, SecondScreenActivity.class);
+
+            //start the parameters activity
+            this.startActivity(intent);
+        }
+    }
+}
